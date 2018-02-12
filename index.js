c7da8a84120cbf911bcbbaf48995d3d0a8e33009
Terri Riggle <codeofarmour@outlook.com>
2018-02-12 19:36:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,3 +15,19 @@ function destructivelyRemoveLastDriver(name) {
 function destructivelyRemoveFirstDriver(name) {
   drivers.shift(name);
 }
+
+function appendDriver(name) {
+  return [...drivers, name];
+}
+
+function prependDriver (name) {
+  return [name, ...drivers];
+}
+
+function removeFirstDriver () {
+  return drivers.slice(1);
+}
+
+function removeLastDriver () {
+  return drivers.slice(0, drivers.length - 1);
+}
